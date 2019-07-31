8fef9d5557e0e4d86ed0a30d10115598ecba2a1e
Md Feroj Bepari <feroj21@gmail.com>
2019-07-31 20:19:51 +0600

    Update DB info

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 class Connection {
-    private  $server = "mysql:host=localhost;dbname=upwork";
-    private  $user = "homestead";
-    private  $pass = "secret";
+    private  $server = "mysql:host=localhost;dbname=notices";
+    private  $user = "root";
+    private  $pass = "";
     
     private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
     protected $con;
